65ec0f309c56589f6d99c4cf37561758c019f15e
Antoine Lehurt <keuwah@gmail.com>
2015-02-28 11:20:28 +0100

    Add npm tasks start & dev

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get('/links/since/:date', function (req, res) {
 });
 
 var server = app.listen(3000, function () {
-
     var host = server.address().address
     var port = server.address().port
+    console.log('App listening at http://%s:%s', host, port)
 });
